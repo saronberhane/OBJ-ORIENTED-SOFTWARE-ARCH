aacf453370012445dd3907ae184f8dff1e1a9195
Saron Berhane <110491957+saronberhane@users.noreply.github.com>
2023-02-26 23:13:32 -0600

    week 6

---
+++ Week 6/StoringObjectsDatabase.py
@@ -0,0 +1,83 @@
+from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, ForeignKey, Table
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+# create engine and session
+engine = create_engine('sqlite:///blog.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# base class for declarative models
+Base = declarative_base()
+
+# create many-to-many relationship between posts and tags
+post_tag_association_table = Table('post_tag_association', Base.metadata,
+    Column('post_id', Integer, ForeignKey('post.id')),
+    Column('tag_id', Integer, ForeignKey('tag.id'))
+)
+
+#user
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    role = Column(String(50), nullable=False, default='reader')
+
+# post 
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    content = Column(Text, nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    author = relationship('User', backref='posts')
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship('Category', backref='posts')
+    tags = relationship('Tag', secondary=post_tag_association_table, backref='posts')
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+# comment
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    content = Column(Text, nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    author = relationship('User')
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    post = relationship('Post', backref='comments')
+    created_at = Column(DateTime, default=datetime.utcnow)
+    status = Column(String(50), nullable=False, default='pending')
+
+# category 
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    description = Column(Text)
+
+# tag 
+class Tag(Base):
+    __tablename__ = 'tag'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    description = Column(Text)
+
+# creating all tables 
+Base.metadata.create_all(engine)
+
+# adding user to database
+def add_user(name, email, password, role='reader'):
+    user = User(name=name, email=email, password=password, role=role)
+    session.add(user)
+    session.commit()
+    return user
+
+# adding post to database
+def add_post(title, content, author_id, category_id=None, tag_ids=None):
+    post = Post(title=title, content=content, author_id=author_id, category_id=category_id)
+    if tag_ids:
+        post.tags = [session.query(Tag).get(tag_id) for tag_id in tag_ids]
---
+++ Week 6/TestDriver.py
@@ -0,0 +1,60 @@
+from StoringObjectsDatabase import Base, User, Post, Tag
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+#  database engine
+engine = create_engine('sqlite:///blog.db')
+
+# database tables
+Base.metadata.create_all(engine)
+
+# csession factory
+Session = sessionmaker(bind=engine)
+
+
+session = Session()
+
+#  users
+saron = User(name='Saron', email='saron@gmail.com')
+berhane = User(name='Berhane', email='berhane@gmail.com')
+zebiba = User(name='Zebiba', email='zebiba@gmail.com')
+
+# add the users to the session
+session.add_all([saron, berhane, zebiba])
+session.commit()
+
+# tags
+python = Tag(name='Python')
+sqlalchemy = Tag(name='SQLAlchemy')
+flask = Tag(name='Flask')
+
+# adding tags to the session
+session.add_all([python, sqlalchemy, flask])
+session.commit()
+
+# creating a post
+post = Post(title='Hello, Everyone!', content='This is my first blog post', author=saron)
+
+# tags to the post
+tag_ids = [python.id, sqlalchemy.id]
+post.tags = [session.query(Tag).get(tag_id) for tag_id in tag_ids]
+
+#post to the session
+session.add(post)
+session.commit()
+
+# query the database
+posts = session.query(Post).all()
+print(posts)
+
+# update post
+post.title = 'My First Blog Post'
+session.commit()
+
+# delete post
+session.delete(post)
+session.commit()
+
+# closing the session
+session.close()
